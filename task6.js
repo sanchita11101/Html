a5040f6c593789e444d36977fcf38aee5aa9d76f
sanchita11101 <sanchitaudawant@gmail.com>
2025-07-25 21:56:26 +0530

    Clone a webpage

---
+++ task6.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("buyBtn").addEventListener("click", function () {
+  alert("Redirecting to purchase page...");
+});
+
+document.getElementById("subscribeForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for subscribing!");
+});
